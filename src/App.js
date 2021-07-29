7c77df6aabfc9544eb223ab78adf1405a8aab800
HMyounus80 <younusm137@gamil.com>
2021-07-29 21:43:12 +0600

    footer section add and finaly done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Feature from "./components/Feature";
-import Hero from "./components/Hero";
-import Products from "./components/Products";
-import { productData, productDataTwo } from "./components/Products/data";
-import { GlobalStyle } from "./globalStyles";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
-    <>
-      <Router>
-        <GlobalStyle />
-        <Hero />
-        <Products heading="Choose your favorite" data={productData} />
-        <Feature />
-        <Products heading="Sweet Treats for You" data={productDataTwo} />
-      </Router>
-    </>
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
+    </Router>
   );
 }
 
