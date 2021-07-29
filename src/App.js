a3b2666028b73dddef03e2764e231c7e2c6ee02d
HMyounus80 <younusm137@gamil.com>
2021-07-29 21:29:55 +0600

    Featur images add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import Feature from "./components/Feature";
 import Hero from "./components/Hero";
-import Products from './components/Products';
-import { productData } from './components/Products/data';
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
 import { GlobalStyle } from "./globalStyles";
-
-
-
-
 
 function App() {
   return (
     <>
-    <Router>
-      <GlobalStyle/>
-      <Hero/>
-      <Products heading='Choose your favorite' data={productData} />
-    </Router>
-      
+      <Router>
+        <GlobalStyle />
+        <Hero />
+        <Products heading="Choose your favorite" data={productData} />
+        <Feature />
+        <Products heading="Sweet Treats for You" data={productDataTwo} />
+      </Router>
     </>
   );
 }
